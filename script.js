46a122d520d9dffa1916959e5ff294cbb7422fe2
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-05-12 10:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
- 
 function calculateMinCost() {
-  const ropeLengths = document.querySelector('#rope-lengths').value.split(',').map(Number);
-
-  // Sort the rope lengths in non-decreasing order
-  ropeLengths.sort((a, b) => a - b);
-
-  let totalCost = 0;
+  // Get the input and validate it
+  const input = document.querySelector('#rope-lengths').value;
+  if (!input) {
+    alert('Please enter a valid input!');
+    return;
+  }
+  const ropeLengths = input.split(',').map(Number);
+  if (ropeLengths.some(isNaN)) {
+    alert('Please enter a valid input!');
+    return;
+  }
 
   // Connect the ropes with minimum length
-  while (ropeLengths.length > 1) {
-    const sum = ropeLengths[0] + ropeLengths[1];
+  const pq = new MinHeap(ropeLengths);
+  let totalCost = 0;
+  while (pq.size() > 1) {
+    const sum = pq.extractMin() + pq.extractMin();
     totalCost += sum;
-
-    // Replace the first two ropes with their sum
-    ropeLengths.splice(0, 2, sum);
-
-    // Re-sort the rope lengths array
-    ropeLengths.sort((a, b) => a - b);
+    pq.insert(sum);
   }
 
   // Print the minimum cost to the result div
@@ -24,3 +25,44 @@ function calculateMinCost() {
   resultDiv.innerHTML = `Minimum cost of connecting ropes: ${totalCost}`;
 }
 
+class MinHeap {
+  constructor(arr) {
+    this.heap = [null];
+    for (const x of arr) {
+      this.insert(x);
+    }
+  }
+
+  insert(x) {
+    this.heap.push(x);
+    let i = this.heap.length - 1;
+    while (i > 1 && this.heap[i] < this.heap[Math.floor(i / 2)]) {
+      [this.heap[i], this.heap[Math.floor(i / 2)]] = [this.heap[Math.floor(i / 2)], this.heap[i]];
+      i = Math.floor(i / 2);
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length <= 1) {
+      return null;
+    }
+    const min = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    let i = 1;
+    while (i * 2 < this.heap.length) {
+      const left = i * 2;
+      const right = i * 2 + 1;
+      const child = right < this.heap.length && this.heap[right] < this.heap[left] ? right : left;
+      if (this.heap[child] >= this.heap[i]) {
+        break;
+      }
+      [this.heap[i], this.heap[child]] = [this.heap[child], this.heap[i]];
+      i = child;
+    }
+    return min;
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+}
